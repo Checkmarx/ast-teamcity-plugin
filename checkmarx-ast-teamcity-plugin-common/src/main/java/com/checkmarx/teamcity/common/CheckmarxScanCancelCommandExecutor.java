d3aadab5710dc2368a58c93f32e57b7bf29c433d
Pedro Lopes <Pedro.Lopes@checkmarx.com>
2022-04-12 11:28:45 +0100

    print information for easy troubleshoot

--- checkmarx-ast-teamcity-plugin-common/src/main/java/com/checkmarx/teamcity/common/CheckmarxScanCancelCommandExecutor.java
+++ checkmarx-ast-teamcity-plugin-common/src/main/java/com/checkmarx/teamcity/common/CheckmarxScanCancelCommandExecutor.java
@@ -1,6 +1,8 @@
 package com.checkmarx.teamcity.common;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import jetbrains.buildServer.TeamCityRuntimeException;
 import jetbrains.buildServer.agent.BuildProgressLogger;
@@ -14,8 +16,23 @@ public class CheckmarxScanCancelCommandExecutor {
       String command = cliPath + " " + CLI_ARGUMENTS + " " + scanID;
       buildProgressLogger.message("Cancelling Checkmarx scan with command: " + command);
       Process process = Runtime.getRuntime().exec(command);
-      process.waitFor();
+      BufferedReader in = new BufferedReader(new
+              InputStreamReader(process.getInputStream()));
+      BufferedReader err = new BufferedReader(new
+              InputStreamReader(process.getErrorStream()));
+      int exitValue = process.waitFor();
+
+      String line = null;
+      while ((line = in.readLine()) != null) {
+        buildProgressLogger.message(line);
+      }
+      while ((line = err.readLine()) != null) {
+        buildProgressLogger.message(line);
+      }
+
+      buildProgressLogger.message("Cancel finish with exit code: " + exitValue);
     } catch (IOException | InterruptedException e) {
+      buildProgressLogger.message("Error canceling: " + e.getMessage());
       throw new TeamCityRuntimeException("Unable to cancel the scan for scanID: " + scanID + ":" + e.getMessage());
     }
    }
