d6ad223fc8ac78ab60ba8db0b456f7857d10d7c4
miryamfoiferCX <miryam.foifer@checkmarx.com>
2025-01-01 13:32:45 +0200

    implement handleRequestInternal

--- checkmarx-ast-teamcity-plugin-server/src/main/java/com/checkmarx/teamcity/server/CheckmarxAdminPageController.java
+++ checkmarx-ast-teamcity-plugin-server/src/main/java/com/checkmarx/teamcity/server/CheckmarxAdminPageController.java
@@ -82,4 +82,8 @@ public class CheckmarxAdminPageController extends BaseFormXmlController {
 
         return errors;
     }
+    @Override
+    protected ModelAndView handleRequestInternal(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response) throws Exception {
+        return null;
+    }
 }
