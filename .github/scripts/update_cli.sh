b7d383091d42b4243480d193a1e8d086ed0e1168
Margarita Levit <45617576+cx-margarita-levitm@users.noreply.github.com>
2025-09-08 10:18:44 +0300

    Update update_cli.sh

--- .github/scripts/update_cli.sh
+++ .github/scripts/update_cli.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
 
 release=$1
+
+# Define file names and URLs for all platforms
 filename_windows=ast-cli_${release}_windows_x64.zip
 filename_linux=ast-cli_${release}_linux_x64.tar.gz
 filename_darwin=ast-cli_${release}_darwin_x64.tar.gz
+
+# Create tmp folder
+mkdir -p tmp
+
+# Create target directory if it doesn't exist
+mkdir -p "../../checkmarx-ast-teamcity-plugin-agent/src/runner/bin/2.0.0"
 
 #Windows
 echo "Updating windows binary"
 wget https://github.com/CheckmarxDev/ast-cli/releases/download/${release}/${filename_windows}
 unzip ${filename_windows} -d tmp
-mv ./tmp/cx.exe ./checkmarx-ast-teamcity-plugin-agent/src/runner/bin/2.0.0/cx.exe
-rm -r tmp
-rm ${filename_windows}
+cp ./tmp/cx.exe ./checkmarx-ast-teamcity-plugin-agent/src/runner/bin/2.0.0/cx.exe
+chmod +x ./checkmarx-ast-teamcity-plugin-agent/src/runner/bin/2.0.0/cx.exe
+rm -r tmp/*()
+rm  ${filename_windows}
 
 #linux
 echo "Updating linux binary"
-wget https://github.com/CheckmarxDev/ast-cli/releases/download/${release}/${filename_linux}
-mkdir ./tmp/
+wget  https://github.com/CheckmarxDev/ast-cli/releases/download/${release}/${filename_linux}
 tar -xvzf  ${filename_linux} -C ./tmp/
-mv ./tmp/cx ./checkmarx-ast-teamcity-plugin-agent/src/runner/bin/2.0.0/cx
-rm -r tmp
-rm ${filename_linux}
+cp ./tmp/cx ./checkmarx-ast-teamcity-plugin-agent/src/runner/bin/2.0.0/cx
+rm -r tmp/*
+rm  ${filename_linux}
 
 #darwin
 echo "Updating mac binary"
 wget https://github.com/CheckmarxDev/ast-cli/releases/download/${release}/${filename_darwin}
-mkdir ./tmp/
 tar -xvzf  ${filename_darwin} -C ./tmp/
-mv ./tmp/cx ./checkmarx-ast-teamcity-plugin-agent/src/runner/bin/2.0.0/cx-mac
+cp ./tmp/cx ./checkmarx-ast-teamcity-plugin-agent/src/runner/bin/2.0.0/cx-mac
 rm -r tmp
 rm ${filename_darwin}
